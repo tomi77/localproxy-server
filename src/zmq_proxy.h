97f4c6b3b57343a5df69946141c28e8f6b2b622a
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-03-06 16:59:36 +0100

    fix h

--- src/zmq_proxy.h
+++ src/zmq_proxy.h
@@ -1,5 +1,10 @@
+#ifndef LOCALPROXY_ZMQ_H__
+#define LOCALPROXY_ZMQ_H__
+
 namespace localproxy {
     namespace zmq_ {
 	void proxy(const char* in, const char* out);
     }
 }
+
+#endif // LOCALPROXY_ZMQ_H__
