cbea58f6d5205536caf79a845b98069ae43ce29f
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-03-06 17:00:03 +0100

    rename env variables

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-    const char* incoming_uri = getenv("ZMQ_LOCAL_INCOMING_BROADCAST_URI");
-    const char* outgoing_uri = getenv("ZMQ_LOCAL_OUTGOING_BROADCAST_URI");
+    const char* incoming_uri = getenv("LOCALPROXY_INCOMING_BROADCAST_URI");
+    const char* outgoing_uri = getenv("LOCALPROXY_OUTGOING_BROADCAST_URI");
 
     localproxy::zmq_::proxy(incoming_uri, outgoing_uri);
 }
